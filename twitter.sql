2e514d14fc76931d3b40e78bc13f628d79a8b220
jaamito <gm.2012.ian.lopez@gmail.com>
2016-11-25 18:48:06 +0100

    1-cambio

---
+++ twitter.sql
@@ -0,0 +1,21 @@
+DROP SCHEMA IF EXISTS twitter;
+CREATE SCHEMA twitter;
+USE twitter;
+SET AUTOCOMMIT=0;
+DROP TABLE IF EXISTS usuarios;
+CREATE TABLE usuarios (
+  `ID` INT(11) NOT NULL AUTO_INCREMENT,
+  `nombre` CHAR(35) NOT NULL DEFAULT '',
+  `apellidos` CHAR(35) NOT NULL DEFAULT '',
+  `email` CHAR(35) NOT NULL DEFAULT '',
+  `tlf` INT(10) NOT NULL,
+  `contraseña` CHAR(20) NOT NULL,
+  `nacimiento` CHAR(20) NOT NULL DEFAULT '',
+  `sexo` CHAR(1) NOT NULL,
+  PRIMARY KEY (`ID`)
+);
+INSERT INTO `usuarios` VALUES (1,'root','root','root@twitteraws2.com',666666666,'root','00/00/00','M');
+INSERT INTO `usuarios` VALUES (2,'ian','lopez zamora','ian@twitteraws2.com',666666666,'ian','20/05/94','M');
+INSERT INTO `usuarios` VALUES (3,'josemi','tudela perez','josemi@twitteraws2.com',666666666,'josemi','06/09/69','M');
+INSERT INTO `usuarios` VALUES (4,'adrian','morales sanagustin','adrian@twitteraws2.com',666666666,'adrian','13/11/95','M');
+COMMIT;
